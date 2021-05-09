237bcbbc6533102570e612fc4da570075b6b762a
Kusal-Bandaru <kusal.bandaru6@gmail.com>
2021-05-09 18:47:36 +0530

    Adding a wrapper component for Card view

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,10 +1,11 @@
 import "./Expenses.css";
 import ExpenseItem from './ExpenseItem';
+import Card from './Card';
 
 function Expenses(props) {
     const items = props.items;
     return (
-        <div className="items">
+        <Card className="expenses">
             <ExpenseItem
                 title={items[0].title}
                 amount={items[0].amount}
@@ -25,7 +26,7 @@ function Expenses(props) {
                 amount={items[3].amount}
                 date={items[3].date}
             ></ExpenseItem>
-        </div>
+        </Card>
     );
 }
 
